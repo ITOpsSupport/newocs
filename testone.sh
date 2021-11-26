a92f565910993e93cfd666bca97071451d642a1e
ITOpsSupport <88940779+ITOpsSupport@users.noreply.github.com>
2021-11-26 15:17:42 +0530

    Update testone.sh

--- testone.sh
+++ testone.sh
@@ -2,6 +2,6 @@ yum remove inventory
 cd /tmp
 wget https://github.com/ITOpsSupport/newocs/blob/main/Centos%206.rpm?raw=true
 rpm -ivh Centos\ 6.rpm
-cd /ocs/inventory/scripts/
+cd /opt/ocsinventory/scripts/
 sh create_crontab.sh
 sh execute_agent.sh
